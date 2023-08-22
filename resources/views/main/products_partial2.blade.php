e26519e9383fb11b11b4f053c3010b884f0e3c5e
Hm95Gaming <mahardikahaikal@gmail.com>
2023-08-22 14:48:08 +0800

    Update products_partial2.blade.php

--- resources/views/main/products_partial2.blade.php
+++ resources/views/main/products_partial2.blade.php
@@ -3,40 +3,42 @@
         <div class="row">
             <!-- ltn__product-item -->
             @foreach($products as $product)
-            @php
-            $produk_name = $product->produk_name;
-            $max_length = 15;
+                @php
+                $produk_name = $product->produk_name;
+                $max_length = 15;
 
-            if (strlen($produk_name) > $max_length) {
-            $produk_name = substr($produk_name, 0, $max_length) . '<span style="color: gray;">...</span>';
-            }
-            @endphp
-            <div class="col-xl-4 col-sm-5 col-5">
-                <div class="ltn__product-item ltn__product-item-3 text-center">
-                    <div class="product-img">
-                        <a href="/produk/{{ $product->id }}" style="display: block;">
-                            @foreach (explode('|', $product->gambar) as $key => $fruit)
-                            @if ($key === 0)
-                            <img src="{{ asset('gambar/'.$fruit) }}" class="card-img-top" style="aspect-ratio: 1 / 1" />
-                            @endif
-                            @endforeach
-                        </a>
-                        <div class="product-badge">
-                            <ul>
-                                <li class="sale-badge" style="background-color: rgb(247, 161, 62);">-{{rand(1, 90)}}%</li>
-                            </ul>
-                        </div>
-                        <div class="product-hover-action">
-                            <ul>
-                                @if(!empty(Auth::user()) && Auth::user()->user_type == 'Pembeli')
-                                <li>
-                                    <a href="#">
-                                        <i class="far fa-heart"></i>
-                                    </a>
-                                </li>
+                if (strlen($produk_name) > $max_length) {
+                $produk_name = substr($produk_name, 0, $max_length) . '<span style="color: gray;">...</span>';
+                }
+                @endphp
+                <div class="col-xl-4 col-sm-5 col-5">
+                    <div class="ltn__product-item ltn__product-item-3 text-center">
+                        <div class="product-img">
+                            <a href="/produk/{{ $product->id }}" style="display: block;">
+                                @foreach (explode('|', $product->gambar) as $key => $fruit)
+                                @if ($key === 0)
+                                <img src="{{ asset('gambar/'.$fruit) }}" class="card-img-top" style="aspect-ratio: 1 / 1" />
                                 @endif
-                            </ul>
+                                @endforeach
+                            </a>
+                            <div class="product-badge">
+                                <ul>
+                                    <li class="sale-badge" style="background-color: rgb(247, 161, 62);">-{{rand(1, 90)}}%</li>
+                                </ul>
+                            </div>
+                            <div class="product-hover-action">
+                                <ul>
+                                    @if(!empty(Auth::user()) && Auth::user()->user_type == 'Pembeli')
+                                    <li>
+                                        <a href="#">
+                                            <i class="far fa-heart"></i>
+                                        </a>
+                                    </li>
+                                    @endif
+                                </ul>
+                            </div>
                         </div>
+<<<<<<< Updated upstream
                     </div>
                     <div class="product-info">
                         
@@ -45,10 +47,29 @@
                         <div class="product-price">
                             <span style="color: rgb(247, 161, 62);">Rp.{{$product->min_price}}</span><br>
                             <s style="color: rgb(215, 40, 40); text-decoration: line-through; text-decoration-style: double;">Rp.{{$product->min_price * 2}}</s>
+=======
+                        <div class="product-info">
+                            <div class="product-ratting">
+                                <ul>
+                                    <!-- Tambahkan logika untuk menampilkan bintang sesuai peringkat produk jika ada -->
+                                    @for ($i = 0; $i < $product->rating; $i++)
+                                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                                        @endfor
+                                        @for ($i = 0; $i < 5 - $product->rating; $i++)
+                                            <li><a href="#"><i class="far fa-star"></i></a></li>
+                                            @endfor
+                                </ul>
+                            </div>
+                            <hr>
+                            <h2 class="product-title"><a href="/produk/{{ $product->id }}">{!! $produk_name !!}</a></h2>
+                            <div class="product-price">
+                                <span style="color: rgb(247, 161, 62);">Rp.{{$product->min_price}}</span><br>
+                                <s style="color: rgb(215, 40, 40); text-decoration: line-through; text-decoration-style: double;">Rp.{{$product->min_price * 2}}</s>
+                            </div>
+>>>>>>> Stashed changes
                         </div>
                     </div>
                 </div>
-            </div>
             @endforeach
             <!--  -->
         </div>
